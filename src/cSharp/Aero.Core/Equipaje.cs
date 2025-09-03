91ae8e76f95ae67d529d6ea3cfd3685f153f77ad
Rodolfo Paetz <Rodolfopaetzet12d1@gmail.com>
2025-09-03 15:18:11 -0300

    Update Equipaje.cs

--- src/cSharp/Aero.Core/Equipaje.cs
+++ src/cSharp/Aero.Core/Equipaje.cs
@@ -4,6 +4,6 @@ public class Equipaje
     public required int IdEquipaje { get; set; }
     public required int IdCheckin { get; set; }
     public required double Peso { get; set; }
-    public required string tipo { get; set; }
+    public required string Tipo { get; set; }
     public required string Etiqueta_codigo { get; set; }
 }
